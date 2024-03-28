7b2e6be6f6c905e9a5a75c80b8e885c0e2bbe5a8
Will <hi@itsgookit.com>
2024-03-28 17:11:03 +0800

    test: update test cases for LoginForm.tsx and App.tsx

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,8 +1,11 @@
 import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import App from "./App";
 
 describe("App", () => {
+  const user = userEvent.setup();
+
   beforeEach(() => {
     render(<App />);
   });
@@ -14,5 +17,28 @@ describe("App", () => {
   it("should render company logo", () => {
     const image = document.querySelector(".company-logo");
     expect(image).toBeInTheDocument();
+  });
+
+  it("should hide the login form and show success message after login success", async () => {
+    const loginForm = screen.getByTestId("login-form");
+    const inputs = {
+      email: "hi@tw.com",
+      password: "@@Zchu777utn",
+    };
+
+    const username = screen.getByPlaceholderText("Email");
+    const password = screen.getByPlaceholderText("Password");
+
+    await user.type(username, inputs.email);
+    await user.type(password, inputs.password);
+
+    const loginButton = screen.getByRole("button", { name: "Submit" });
+    await user.click(loginButton);
+
+    expect(loginForm).not.toBeInTheDocument();
+
+    const success = `Welcome to MyWAVE.AI`;
+    const successMessage = screen.queryByText(success);
+    expect(successMessage).toBeInTheDocument();
   });
 });
--- src/components/LoginForm.spec.tsx
+++ src/components/LoginForm.spec.tsx
@@ -1,15 +1,30 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { LoginForm } from "./LoginForm";
 
 describe("LoginForm", () => {
+  const user = userEvent.setup();
+  const onSubmit = vi.fn();
   beforeEach(() => {
-    render(<LoginForm />);
+    render(<LoginForm onSubmit={onSubmit} />);
   });
 
-  it("should disabled the submit button at the beginning", () => {
+  it("should ensure input values are required.", async () => {
+    const emailMessage = "The email is required.";
+    const passwordMessage = "The password is required.";
+    const confirmMessage = "The confirm password is required.";
     const loginButton = screen.getByRole("button", { name: "Submit" });
-    expect(loginButton).toBeDisabled();
+    await user.click(loginButton);
+
+    const emailError = screen.queryByText(emailMessage);
+    expect(emailError).toBeInTheDocument();
+
+    const passwordError = screen.queryByText(passwordMessage);
+    expect(passwordError).toBeInTheDocument();
+
+    const confirmPasswordError = screen.queryByText(confirmMessage);
+    expect(confirmPasswordError).toBeInTheDocument();
   });
 
   it("should initialize an empty form with inputs that are all required", () => {
@@ -20,17 +35,75 @@ describe("LoginForm", () => {
     expect(email).toHaveValue("");
     expect(password).toHaveValue("");
     expect(confirmPassword).toHaveValue("");
-
-    expect(email).toBeRequired();
-    expect(password).toBeRequired();
-    expect(confirmPassword).toBeRequired();
   });
 
-  it("should should be required for inputs", () => {});
+  it("should modify input values", async () => {
+    const emailValue = "incorrect.email";
+    const email = screen.getByPlaceholderText("Email");
+    await user.type(email, emailValue);
+    expect(email).toHaveValue(emailValue);
 
-  it("should prevent submit function from being called if invalid", () => {});
+    const pwValue = "password";
+    const password = screen.getByPlaceholderText("Password");
+    await user.type(password, pwValue);
+    expect(password).toHaveValue(pwValue);
 
-  it("should enable submit button if inputs are all valid", () => {});
+    const cpwValue = "password";
+    const confirmPassword = screen.getByPlaceholderText("Confirm password");
+    await user.type(confirmPassword, cpwValue);
+    expect(confirmPassword).toHaveValue(cpwValue);
+  });
 
-  it("should display error message for invalid inputs", () => {});
+  it("should display error message if email format is incorrect", async () => {
+    const emailContent = "incorrect.email";
+    const email = screen.getByPlaceholderText("Email");
+    await user.type(email, emailContent);
+    expect(email).toBeInvalid();
+
+    const errorMessage = screen.queryByText("Email format is incorrect.");
+    expect(errorMessage).toBeInTheDocument();
+  });
+
+  it("should display error message if password format is incorrect", async () => {
+    const message = `must have at least one capital letter, one numeric character, and one special character.`;
+    const pwContent = "password";
+    const password = screen.getByPlaceholderText("Password");
+    expect(password).toBeInTheDocument();
+    await user.type(password, pwContent);
+
+    const errorMessage = screen.queryByText(message);
+    expect(errorMessage).toBeInTheDocument();
+  });
+
+  it("should display error message if password is not equal to confirm password", async () => {
+    const message = "The password is inconsistent.";
+    const pwContent = "password";
+    const password = screen.getByPlaceholderText("Password");
+    await user.type(password, pwContent);
+
+    const cpwContent = "password!!";
+    const confirmPassword = screen.getByPlaceholderText("Confirm password");
+    await user.type(confirmPassword, cpwContent);
+
+    const errorMessage = screen.queryByText(message);
+    expect(errorMessage).toBeInTheDocument();
+  });
+
+  it("should passing email and password.", async () => {
+    const inputs = {
+      email: "hi@tw.com",
+      password: "@@Zchu777utn",
+    };
+
+    const username = screen.getByPlaceholderText("Email");
+    const password = screen.getByPlaceholderText("Password");
+
+    await user.type(username, inputs.email);
+    await user.type(password, inputs.password);
+
+    const loginButton = screen.getByRole("button", { name: "Submit" });
+    await user.click(loginButton);
+
+    expect(onSubmit).toHaveBeenCalledWith(inputs);
+  });
 });
