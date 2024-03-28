94c162cc1fe22a7e4fdb313a8eb39706b578adcd
Will <hi@itsgookit.com>
2024-03-28 17:14:55 +0800

    feat: implment the App.tsx and LoginForm.tsx that pass test cases

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
+import { LoginForm } from "./components/LoginForm";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
   return (
     <>
       <div data-testid="login-app">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://mywave.ai" target="_blank">
+          <img
+            src={
+              "https://mywave.ai/wp-content/uploads/2022/10/mywave-logo-white.svg"
+            }
+            className="company-logo"
+            alt="Company logo"
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {!isLoggedIn ? (
+        <LoginForm
+          onSubmit={(inputs: any) => {
+            Promise.resolve().then(() => {
+              setIsLoggedIn(true);
+            });
+          }}
+        />
+      ) : (
+        <span>Welcome to MyWAVE.AI</span>
+      )}
     </>
   );
 }
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,15 +1,125 @@
-interface LoginFormProps {}
+import { useState } from "react";
+
+interface onSubmitProps {
+  email: string;
+  password: string;
+}
+interface LoginFormProps {
+  onSubmit: (props: onSubmitProps) => void;
+}
 
 export function LoginForm(props: LoginFormProps) {
+  const { onSubmit } = props;
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+
+  const [emailError, setEmailError] = useState<string>("");
+  const [passwordError, setPasswordError] = useState<string>("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState<string>("");
+
   return (
     <div>
-      <form>
-        <input type="email" placeholder="Email"></input>
-        <input type="password" placeholder="Password"></input>
-        <input type="password" placeholder="Confirm password"></input>
-        <button type="submit" disabled>
-          Submit
-        </button>
+      <form
+        data-testid="login-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          const emailRegExp =
+            /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+          if (!email) {
+            setEmailError("The email is required.");
+          } else if (!emailRegExp.test(email)) {
+            setEmailError("Email format is incorrect.");
+          } else {
+            setEmailError("");
+          }
+
+          const passwordRegExp = /^(?=.*[A-Z])(?=.*\d)(?=.*\W).+$/;
+
+          if (!password) {
+            setPasswordError("The password is required.");
+          } else if (!passwordRegExp.test(password)) {
+            setPasswordError(
+              `must have at least one capital letter, one numeric character, and one special character.`
+            );
+          } else {
+            setPasswordError("");
+          }
+
+          if (!confirmPassword) {
+            setConfirmPasswordError("The confirm password is required.");
+          } else if (password !== confirmPassword) {
+            setConfirmPasswordError("The password is inconsistent.");
+          } else {
+            setConfirmPasswordError("");
+          }
+
+          onSubmit({
+            email,
+            password,
+          });
+        }}
+      >
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => {
+            const email = e.target.value;
+            setEmail(e.target.value);
+            const emailRegExp =
+              /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+            if (!email) {
+              setEmailError("The email is required.");
+            } else if (!emailRegExp.test(email)) {
+              setEmailError("Email format is incorrect.");
+            } else {
+              setEmailError("");
+            }
+          }}
+        ></input>
+        <span> {emailError}</span>
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => {
+            const password = e.target.value;
+            setPassword(password);
+            const passwordRegExp = /^(?=.*[A-Z])(?=.*\d)(?=.*\W).+$/;
+
+            if (!password) {
+              setPasswordError("The password is required.");
+            } else if (!passwordRegExp.test(password)) {
+              setPasswordError(
+                `must have at least one capital letter, one numeric character, and one special character.`
+              );
+            } else {
+              setPasswordError("");
+            }
+          }}
+        ></input>
+        <span> {passwordError}</span>
+        <input
+          type="password"
+          placeholder="Confirm password"
+          value={confirmPassword}
+          onChange={(e) => {
+            setConfirmPassword(e.target.value);
+            const confirmPassword = e.target.value;
+            if (!confirmPassword) {
+              setConfirmPasswordError("The confirm password is required.");
+            } else if (password !== confirmPassword) {
+              setConfirmPasswordError("The password is inconsistent.");
+            } else {
+              setConfirmPasswordError("");
+            }
+          }}
+        ></input>
+        <span>{confirmPasswordError}</span>
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
